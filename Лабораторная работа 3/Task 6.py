30a80c0fb870c1831569dc12b59a1b693bdd0720
shadowmoon0 <115100049+shadowmoon0@users.noreply.github.com>
2022-10-14 01:32:10 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/Task 6.py
@@ -0,0 +1,6 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+m = list_numbers.index(max(list_numbers))
+list_numbers[-1],list_numbers[m] = list_numbers[m],list_numbers[-1]
+
+print(list_numbers)
---
+++ Лабораторная работа 3/Task 9.py
@@ -0,0 +1,12 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+while months>0:
+    need_money+=spend-salary
+    spend+=spend*0.03
+    months-=1
+print(round(need_money))
